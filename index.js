7467220a501448f2fd417dcf275a04ba4b87121d
Max Kitsch <max.kitsch@gmail.com>
2016-10-13 16:31:43 +0300

    Options are passed as a second param to bugsnag.register

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bugsnag = require("bugsnag");
 const BrestBugsnag = {
 
     init: function(brest, callback) {
-        bugsnag.register(brest.getSetting('bugsnag.key'));
+        bugsnag.register(brest.getSetting('bugsnag.key'), brest.getSetting('bugsnag.options'));
         brest.getApp().use(bugsnag.requestHandler);
         brest.getApp().use(bugsnag.errorHandler);
         callback();
